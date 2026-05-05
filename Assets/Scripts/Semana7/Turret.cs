6d69caf10ec4164690963217a3d39d5c40c0b91d
abelobregon-design <diego.apena@tecsup.edu.pe>
2026-05-05 12:40:56 -0500

    TORRETA

--- Assets/Scripts/Semana7/Turret.cs
+++ Assets/Scripts/Semana7/Turret.cs
@@ -1,24 +1,73 @@
 using UnityEngine;
-
-public class Turret : MonoBehaviour
+using UnityEngine.Events;
+using Sirenix.OdinInspector;
+using System.Collections.Generic;
+public class MuzzleController : MonoBehaviour
 {
-    public GameObject HeadTurret;
-    public GameObject Enemy;
+    public List<int> ListaEnemigos;
+
+    public Transform Enemy;
+    public GameObject BulletPrefab;
+    public GameObject headTurret;
+    public Transform FirePointRight;
+    public Transform FirePointLeft;
+
     public float rotationSpeed;
+    public float BulletSpeed = 20f;
+
+    public float RatioDetection = 10;
+    private float fireRate = 1f;
+    private float nextFireTime = 0f;
+    private bool useRightFirePoint = true;
+
+    public ParticleSystem ShootParticlesLeft;
+    public ParticleSystem ShootParticlesRight;
     void Start()
     {
-        Enemy = GameObject.FindGameObjectWithTag("Enemy");
+
     }
 
-    
     void Update()
     {
-        Rotate();
+        Detection();
+        Shoot();
     }
-    public void Rotate()
+
+    public void Detection()
     {
-        Vector3 Head = (Enemy.transform.position - transform.position).normalized;
-        Quaternion targetQuaternion = Quaternion.LookRotation(Head);
-        HeadTurret.transform.rotation = Quaternion.Slerp(HeadTurret.transform.rotation,targetQuaternion,rotationSpeed * Time.deltaTime);
+        if (Enemy == null) return;
+
+        Vector3 HeadDir = (Enemy.transform.position - transform.position);
+
+        Quaternion targetQuaternion = Quaternion.LookRotation(HeadDir);
+
+        headTurret.transform.rotation = Quaternion.Slerp(headTurret.transform.rotation, targetQuaternion, rotationSpeed * Time.deltaTime);
+
+        float distanceToEnemy = Vector3.Distance(transform.position, Enemy.transform.position);
+        if (distanceToEnemy > 15f) return;
+    }
+    public void Shoot()
+    {
+        if (Time.time >= nextFireTime)
+        {
+            nextFireTime = Time.time + fireRate;
+
+            Transform currentFirePoint = useRightFirePoint ? FirePointRight : FirePointLeft;
+            ParticleSystem currentParticles = useRightFirePoint ? ShootParticlesRight : ShootParticlesLeft;
+            useRightFirePoint = !useRightFirePoint;
+
+            Debug.Log("Torreta Dispara");
+            currentParticles.Play();
+
+            GameObject bullet = Instantiate(BulletPrefab, currentFirePoint.position, currentFirePoint.rotation);
+
+            Rigidbody rb = bullet.GetComponent<Rigidbody>();
+
+            if (rb != null)
+            {
+                rb.linearVelocity = currentFirePoint.forward * BulletSpeed;
+            }
+            Destroy(bullet, 3f);
+        }
     }
 }
